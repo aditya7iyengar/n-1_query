75924ab48e32f3edd0281649e473ab0d7403b19c
aditya7iyengar <adi.iyengar@annkissam.com>
2016-07-08 09:39:17 -0400

    Add completed example

--- app/models/Tester.rb
+++ app/models/Tester.rb
@@ -1,9 +1,54 @@
 class Tester
-  def self.print_client_names_and_zip_codes(limit=10)
+  def self.print_client_names_and_zip_codes(limit)
     clients = Client.limit(limit)
 
     clients.each do |client|
-      puts "#{client.name} | #{client.address_record.zip_code}"
+      name = client.name
+      zip_code = client.address_record.zip_code
+    end
+  end
+
+  def self.benchmark_client_names_and_zip_codes(limit=999)
+    time_recorder = TimeRecorder.new
+    time_recorder.benchmark do
+      print_client_names_and_zip_codes(limit)
+    end
+  end
+
+  def self.print_client_names_and_zip_codes_improved(limit)
+    clients = Client.includes(:address_record).limit(limit)
+
+    clients.each do |client|
+      name = client.name
+      zip_code = client.address_record.zip_code
+    end
+  end
+
+  def self.benchmark_client_names_and_zip_codes_improved(limit=999)
+    time_recorder = TimeRecorder.new
+    time_recorder.benchmark do
+      print_client_names_and_zip_codes_improved(limit)
+    end
+  end
+
+  # Tester.benchmark_client_names_and_zip_codes
+  # Tester.benchmark_client_names_and_zip_codes_improved
+  # Tester.populator(1000, 1000)
+  # ActiveRecord::Base.logger = nil
+
+  def self.populator(client_max=100, address_record_max=100)
+    client_index = 0
+    loop do
+      Client.create(name: "Client#{client_index}")
+      client_index += 1
+      break if client_index >= client_max
+    end
+
+    address_record_index = 0
+    loop do
+      AddressRecord.create(zip_code: "#{(address_record_index * address_record_index).to_s}", client_id: address_record_index)
+      address_record_index += 1
+      break if address_record_index >= address_record_max
     end
   end
 end
--- app/models/time_recorder.rb
+++ app/models/time_recorder.rb
@@ -3,8 +3,8 @@ class TimeRecorder
   end
 
   def benchmark(options={})
-    start_time = (Time.now * 1000).to_i
-    puts "start time: #{start_time} ms"
+    start_time = Time.now
+    puts "start time: #{start_time}"
     puts "--------------------------------------------------------------"
     puts "--------------------------------------------------------------"
     puts "--------------------------------------------------------------"
@@ -16,19 +16,20 @@ class TimeRecorder
       raise e
     ensure
 
-    puts "--------------------------------------------------------------"
-    puts "--------------------------------------------------------------"
-    puts "--------------------------------------------------------------"
+      puts "--------------------------------------------------------------"
+      puts "--------------------------------------------------------------"
+      puts "--------------------------------------------------------------"
 
-    end_time = (Time.now * 1000).to_i
-    puts "end time: #{end_time} ms"
+      end_time = Time.now
+      puts "end time: #{end_time} ms"
 
-    if error.present?
-      puts "error: #{error}"
+      if error.present?
+        puts "error: #{error}"
+      end
+
+      total_time = ((end_time - start_time) * 1000).to_i
+
+      puts "Total time: #{total_time} ms"
     end
-
-    total_time = ((end_time - start_time) * 1000).to_i
-
-    puts "Total time: #{total_time} ms"
   end
 end
